b1770db97e7053f1e45300bde548479316f3a31d
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-08 17:59:25 -0500

    Se agrega el archivo db.js para conectarse al mongoDB Atlas.

--- config/db.js
+++ config/db.js
@@ -2,9 +2,16 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
     try {
-        const connection = await mongoose.connet()
+        const db = process.env.DATABASE_URI;
+        const connection = await mongoose.connect(db);
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+
+        console.log(`MongoDB conectado en: ${url}`);
+
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1);
     }
 }
+
+export default conectarDB;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import express from "express"; //No necesita extension .js
+import express from "express";
 import dotenv from 'dotenv';
+import conectarDB from "./config/db.js";
+
 const app = express();
 dotenv.config();
+
+conectarDB(); 
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
