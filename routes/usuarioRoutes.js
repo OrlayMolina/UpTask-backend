a191ed54504fff0022a68a9edf26fbd5fb7c120f
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-08 21:51:57 -0500

    Se crea el método para actualizar la contraseña.

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
 
-import { registrar, autenticar, confirmar, olvidePassword, comprobarToken } from "../controllers/usuarioController.js"; //Archivos propios, especificar .js
+import { registrar, autenticar, confirmar, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/usuarioController.js"; //Archivos propios, especificar .js
 
 // Autenticaión, Registro y Confirmación de Usuarios.
 router.post('/', registrar); //Crea un nuevo usuario.
 router.post('/login', autenticar);
 router.get('/confirmar/:token', confirmar);
 router.post('/olvide-password', olvidePassword);
-router.get('/olvide-password/:token', comprobarToken);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
 export default router;
