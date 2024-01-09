90021483084a75dcbb5d8529ce0a01d3ec17fe0c
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-08 20:54:42 -0500

    Se agrega jsonwebtoken para generar un token despues de confirmar el login.

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,6 @@
 import Usuario from '../models/Usuario.js';
+import generarId from '../helpers/generarId.js';
+import generarJWT from '../helpers/generarJWT.js';
 
 const registrar = async (req, res) => {
     //Evitar registros duplicados (Front)
@@ -13,6 +15,7 @@ const registrar = async (req, res) => {
     try {
 
         const usuario = new Usuario(req.body);  
+        usuario.token = generarId();
         const usuarioAlmacenado = await usuario.save();
         res.status(200).json({
             msg: 'Usuario creado correctamente.',
@@ -29,6 +32,37 @@ const registrar = async (req, res) => {
     }
 };
 
+const autenticar = async (req, res) => {
+    const { email, password } = req.body;
+    //Comprobar que existe
+    const usuario = await Usuario.findOne({email});
+    if(!usuario){
+        const error = new Error('Usuario no existe.');
+        return res.status(404).json({ msg: error.message });
+    }
+
+    //Comprobar si esta confirmado
+    if(!usuario.confirmado){
+        const error = new Error('Tu cuenta no ha sido confirmada.');
+        return res.status(403).json({ msg: error.message });
+    }
+
+    //Comprobar password
+    if(await usuario.comprobarPassword(password)){
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario._id)
+        })
+    }else {
+        const error = new Error('El password es incorrecto.');
+        return res.status(403).json({ msg: error.message });
+    }
+
+}
+
 export {
-    registrar
+    registrar,
+    autenticar
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { registrar} from "../controllers/usuarioController.js"; //Archivos propios, especificar .js
+import { registrar, autenticar } from "../controllers/usuarioController.js"; //Archivos propios, especificar .js
 
 // Autenticaión, Registro y Confirmación de Usuarios.
 router.post('/', registrar); //Crea un nuevo usuario.
+router.post('/login', autenticar);
 
 export default router;
